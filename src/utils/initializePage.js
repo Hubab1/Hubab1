8a06184593c310ff481320ea50fadd98edd65ec5
pawlkris <paul.kristapovich@nestio.com>
2019-04-22 17:10:44 -0400

    move RentalProfileOptions

---
+++ src/utils/initializePage.js
@@ -0,0 +1,11 @@
+import history from './history';
+
+
+export const initializePage = (profile) => {
+    if (!profile) return;
+    if (!profile.completed_terms_and_conditions) {
+        history.push('/terms');
+    } else if (profile.selected_rental_options.length === 0) {
+        history.push('/profile/options');
+    }
+}
