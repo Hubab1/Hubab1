e256947137eb2f6293fef94e94a7a1e8895c65bd
scott dikowitz <dikowitz@gmail.com>
2019-04-23 16:14:40 -0400

    move stuff around

--- src/components/main/RenterProfileOptions.jsx
+++ src/components/main/RenterProfileOptions.jsx
@@ -2,69 +2,74 @@ import React, { Fragment } from 'react';
 import { Formik } from 'formik';
 
 import Page from './Page';
-import MultiSelectChoice from './MultiSelectChoice';
-import MultiSelect from './MultiSelectChoice/MultiSelect';
+import MultiSelectChoice from './MultiSelect/MultiSelectChoice';
+import MultiSelect from './MultiSelect';
 import { PageHeader, Subtitle } from 'assets/index';
 
 export default class RentalProfileOptions extends React.Component {
+    onSubmit = () => {
+        // todo
+    }
+    
     render () {
         return (
             <Page>
                 <div>
-                <Formik
-                    initialValues={{ options: [] }}
-                    validate={values => {
-                        let errors = {};
-                        return errors;
-                    }}
-                    onSubmit={this.onSubmit}
-                >
-                {({
-                    values,
-                    setFieldValue,
-                }) => (
-                    <Fragment>
-                        <PageHeader>Let's talk about your new place</PageHeader>
-                        <Subtitle>Select all that apply</Subtitle>
-                        <div style={{height: 12}}></div>
-                        <MultiSelect
-                            onChange={(value) => setFieldValue('options', value)}
-                            value={values.options}
-                        >
-                            <MultiSelectChoice
-                                prefix="👪"
-                                name="other_adults"
-                                label="Other adults will live here"
-                            />
-                            <div style={{height: 8}}></div>
-                            <MultiSelectChoice
-                                prefix="🐶"
-                                name="pets"
-                                label="Pets will live here"
-                            />
-                            <div style={{height: 8}}></div>
-                            <MultiSelectChoice
-                                prefix="💰"
-                                name="guarantor"
-                                label="I'll need a guarantor"
-                            />
-                            <div style={{height: 8}}></div>
-                            <MultiSelectChoice
-                                prefix="🚗"
-                                name="parking"
-                                label="I'd like a parking space"
-                            />
-                            <div style={{height: 8}}></div>
-                            <MultiSelectChoice
-                                prefix="🛍️"
-                                name="storage"
-                                label="I'll need extra storage"
-                            />
-                            <div style={{height: 8}}></div>
-                        </MultiSelect>
-                    </Fragment>
-                )
-                }</Formik>
+                    <Formik
+                        initialValues={{ options: [] }}
+                        validate={values => {
+                            let errors = {};
+                            // todo
+                            return errors;
+                        }}
+                        onSubmit={this.onSubmit}
+                    >
+                    {({
+                        values,
+                        setFieldValue
+                    }) => (
+                        <Fragment>
+                            <PageHeader>Let's talk about your new place</PageHeader>
+                            <Subtitle>Select all that apply</Subtitle>
+                            <div style={{height: 12}}></div>
+                            <MultiSelect
+                                onChange={(value) => setFieldValue('options', value)}
+                                value={values.options}
+                            >
+                                <MultiSelectChoice
+                                    prefix="👪"
+                                    name="other_adults"
+                                    label="Other adults will live here"
+                                />
+                                <div style={{height: 8}}></div>
+                                <MultiSelectChoice
+                                    prefix="🐶"
+                                    name="pets"
+                                    label="Pets will live here"
+                                />
+                                <div style={{height: 8}}></div>
+                                <MultiSelectChoice
+                                    prefix="💰"
+                                    name="guarantor"
+                                    label="I'll need a guarantor"
+                                />
+                                <div style={{height: 8}}></div>
+                                <MultiSelectChoice
+                                    prefix="🚗"
+                                    name="parking"
+                                    label="I'd like a parking space"
+                                />
+                                <div style={{height: 8}}></div>
+                                <MultiSelectChoice
+                                    prefix="🛍️"
+                                    name="storage"
+                                    label="I'll need extra storage"
+                                />
+                                <div style={{height: 8}}></div>
+                            </MultiSelect>
+                        </Fragment>
+                    )
+                    }</Formik>
                 </div>
             </Page>
         );
