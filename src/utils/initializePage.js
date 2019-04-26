1e030793162c59425ecf1313371c7a4be6735329
scott dikowitz <dikowitz@gmail.com>
2019-04-26 14:54:47 -0400

    flog the log

--- src/utils/initializePage.js
+++ src/utils/initializePage.js
@@ -3,7 +3,6 @@ import history from 'history.js';
 
 export const initializePage = (profile) => {
     if (!profile) return;
-    console.log(profile)
     if (!profile.completed_terms_and_conditions) {
         history.push('/terms');
     } else if (profile.selected_rental_options.length === 0) {
