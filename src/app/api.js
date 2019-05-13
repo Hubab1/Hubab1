790593fc15e793b3c6f03e1f5b65086c2be03552
pawlkris <paul.kristapovich@nestio.com>
2019-05-13 12:36:57 -0400

    add tests and documentation

--- src/app/api.js
+++ src/app/api.js
@@ -5,7 +5,7 @@ const getWithHeaders = (url) => fetch(url, {
         "Content-Type": "application/json",
         "Accept": "application/json"
     }
-}).then(res => res.json()).then(data => data);
+}).then(res => res.json());
 
 const CHUCK_BASE_LEASE_SETTINGS = (communityId) => `${CHUCK_BASE_URL}/api/onlineleasing/communities/${communityId}`;
 const CHUCK_PERSONALIZED_LEASE_SETTINGS = (communityId, hash) => `${CHUCK_BASE_URL}/api/onlineleasing/communities/${communityId}/personalized/${hash}`;
@@ -19,36 +19,6 @@ API.fetchConfiguration = (communityId) => {
 API.fetchPersonalizedInfo = (communityId, hash) => {
     return getWithHeaders(CHUCK_PERSONALIZED_LEASE_SETTINGS(communityId, hash));
 };
-
-
-// const invalidCommunitites = new Set(['23', '44', '25']);
-// if (invalidCommunitites.has(communityId)) return Promise.reject({ error: 'invalid community' });
-
-// const payload = {
-//     "name": "Cortland",
-//     "street": "2173 Highland Ave S",
-//     "city": "Birmingham",
-//     "state": "AL",
-//     "postal_code": 35205,
-//     "terms_of_service": "https://some-link.com/something.pdf",
-//     "privacy_policy": "https://some-link.com/something.pdf",
-//     "logo": logoImage,
-//     "background": backgroundImage,
-//     "primary_color": "rgba(40,97,101,1)",
-//     "secondary_color": "#FFFFFF",
-//     "unit_number": "3B",
-//     "monthly_rent": 5000,
-// };
-// if (hash) {
-//     payload.client = {
-//         "first_name": "Chester",
-//         "last_name": "Cheetah",
-//         "email": "chester@cheetos.com",
-//         "phone": "+11231231234",
-//     };
-// }
-
-// return Promise.resolve(payload);
 
 API.fetchRenterProfile = () => {
     return Promise.resolve({
