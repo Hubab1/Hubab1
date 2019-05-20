512fac2db4aa0735138de34c3fd77d7d728a69bb
scott dikowitz <dikowitz@gmail.com>
2019-05-20 17:30:11 -0400

    create guarantorpage

--- src/components/profile/ProfileContainer.jsx
+++ src/components/profile/ProfileContainer.jsx
@@ -3,13 +3,14 @@ import { Route, Switch } from 'react-router-dom';
 
 import { ROUTES } from 'app/constants';
 import RenterProfileOptions from 'components/profile/options/RenterProfileOptions';
-
+import GuarantorPage from 'components/profile/options/GuarantorPage';
 
 class ProfileContainer extends Component {
     render() {
         return (
             <Switch>
                 <Route path={ROUTES.PROFILE_OPTIONS} component={RenterProfileOptions} />
+                <Route path={ROUTES.GUARANTOR} component={GuarantorPage} />
             </Switch>
         );
     }
