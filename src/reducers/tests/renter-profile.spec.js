0f59ef28049cc30b19f3365a561f8e1472c36caa
scott dikowitz <dikowitz@gmail.com>
2019-06-07 12:15:33 -0400

    uncomment tests

--- src/reducers/tests/renter-profile.spec.js
+++ src/reducers/tests/renter-profile.spec.js
@@ -11,45 +11,45 @@ describe('selectInitialPage', () => {
             }
         };
         let initialPage;
-        // initialPage = selectors.selectInitialPage({
-        //     configuration,
-        //     renterProfile: {
-        //         selected_rental_options: ['roommates', 'pets'],
-        //         roommates: null,
-        //         pets: null
-        //     }
-        // });
-        // expect(initialPage).toEqual(ROUTES.ROOMMATES);
-        // initialPage = selectors.selectInitialPage({
-        //     configuration,
-        //     renterProfile: {
-        //         selected_rental_options: ['roommates', 'pets'],
-        //         roommates: [{name: 'bob'}],
-        //         pets: null
-        //     }
-        // });
-        // expect(initialPage).toEqual(ROUTES.PETS);
-        // initialPage = selectors.selectInitialPage({
-        //     configuration,
-        //     renterProfile: {
-        //         selected_rental_options: ['roommates', 'pets'],
-        //         roommates: [{name: 'bob'}],
-        //         pets: [{name: 'Luscious', breed: 'Pitty', weight: '99', pet_type: 'Dog'}, {name: 'garfield', pet_type: 'Cat'}]
-        //     }
-        // });
-        // expect(initialPage).toEqual(ROUTES.PETS);
+        initialPage = selectors.selectInitialPage({
+            configuration,
+            renterProfile: {
+                selected_rental_options: ['roommates', 'pets'],
+                roommates: null,
+                pets: null
+            }
+        });
+        expect(initialPage).toEqual(ROUTES.ROOMMATES);
+        initialPage = selectors.selectInitialPage({
+            configuration,
+            renterProfile: {
+                selected_rental_options: ['roommates', 'pets'],
+                roommates: [{name: 'bob'}],
+                pets: null
+            }
+        });
+        expect(initialPage).toEqual(ROUTES.PETS);
+        initialPage = selectors.selectInitialPage({
+            configuration,
+            renterProfile: {
+                selected_rental_options: ['roommates', 'pets'],
+                roommates: [{name: 'bob'}],
+                pets: [{name: 'Luscious', breed: 'Pitty', weight: '99', pet_type: 'Dog'}, {name: 'garfield', pet_type: 'Cat'}]
+            }
+        });
+        expect(initialPage).toEqual(ROUTES.PETS);
 
-        // initialPage = selectors.selectInitialPage({
-        //     configuration,
-        //     renterProfile: {
-        //         selected_rental_options: ['guarantor', 'pets'],
-        //         roommates: null,
-        //         guarantor: null,
-        //         pets: null
-        //     }
-        // });
+        initialPage = selectors.selectInitialPage({
+            configuration,
+            renterProfile: {
+                selected_rental_options: ['guarantor', 'pets'],
+                roommates: null,
+                guarantor: null,
+                pets: null
+            }
+        });
 
-        // expect(initialPage).toEqual(ROUTES.GUARANTOR);
+        expect(initialPage).toEqual(ROUTES.GUARANTOR);
 
         initialPage = selectors.selectInitialPage({
             configuration,
