c38f162511dfd0c48cf51b7d1c612b4beb9714a3
pawlkris <paul.kristapovich@nestio.com>
2019-06-12 12:31:39 -0400

    ugly version

--- src/components/ConnectBankPage.jsx
+++ src/components/ConnectBankPage.jsx
@@ -1,14 +1,29 @@
 import React, { Fragment } from 'react';
 import styled from '@emotion/styled';
+import { css } from 'emotion';
 import { Link } from 'react-router-dom';
+import DoneRoundedIcon from '@material-ui/icons/DoneRounded';
 
+import safeImage from 'assets/images/connect-bank/safe.png';
+import padlockImage from 'assets/images/connect-bank/padlock.png';
 import ActionButton from 'components/common/ActionButton/ActionButton';
-import { H1, H3, linkRoot } from 'assets/styles';
+import { H1, H3, P, Bold, linkRoot } from 'assets/styles';
 import { ROUTES } from 'app/constants';
 
 const SpacedH3 = styled(H3)`
     margin: 20px 5% 25px 5%;
 `
+
+const bodyContainer = css`
+    margin: 40px 0;
+    display: flex;
+`
+
+const bodyRow = css`
+    display: flex;
+    justify-content: center;
+`
+
 
 export class ConnectBankPage extends React.Component {
 
@@ -21,6 +36,17 @@ export class ConnectBankPage extends React.Component {
             <Fragment>
                 <H1>Verify Your Income Instantly</H1>
                 <SpacedH3>Confirm that you qualify for this apartment by linking your bank account.</SpacedH3>
+                <img src={safeImage} alt="valult"/>
+                <div className={bodyContainer}>
+                    <div className={bodyRow}>
+                        <img src={padlockImage} alt="padlock"/>
+                        <P><Bold>Your data is safe.</Bold> Bank level encryption is used to connect and your credentials are never stored.</P>
+                    </div>
+                    <div className={bodyRow}>
+                        <DoneRoundedIcon style={{color:'#00CAB1', width:'18px'}}/>
+                        <P><Bold>Your money is safe.</Bold> This does not authorize any transactions from your account.</P>
+                    </div>
+                </div>
                 <ActionButton onClick={this.linkBankAccount} marginTop="20px" marginBottom="20px">
                     Link Bank Account
                 </ActionButton>
