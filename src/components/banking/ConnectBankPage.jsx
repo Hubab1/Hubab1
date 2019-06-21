2bf9b8aa15c9709186d38fb0bb88d9e0fb3d9659
pawlkris <paul.kristapovich@nestio.com>
2019-06-21 15:06:32 -0400

    fix copy on verify income page and error on reset password code page

--- src/components/banking/ConnectBankPage.jsx
+++ src/components/banking/ConnectBankPage.jsx
@@ -1,13 +1,13 @@
 import React, { Fragment } from 'react';
 import styled from '@emotion/styled';
 import { css } from 'emotion';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import DoneRoundedIcon from '@material-ui/icons/DoneRounded';
 
 import safeImage from 'assets/images/connect-bank/safe.png';
 import padlockImage from 'assets/images/connect-bank/padlock.png';
 import ActionButton from 'components/common/ActionButton/ActionButton';
-import { H1, H3, P, Bold, linkRoot } from 'assets/styles';
+import { H1, H3, P, Bold } from 'assets/styles'; // add back linkRoot when we add LINK back
 import { ROUTES } from 'app/constants';
 import GenericFormError from 'components/common/GenericFormError';  
 import API from 'app/api';
@@ -75,7 +75,7 @@ export class ConnectBankPage extends React.Component {
         }
         return (
             <Fragment>
-                <H1>Verify Your Income Instantly</H1>
+                <H1>Verify Your Income & Assets Instantly</H1>
                 <SpacedH3>Confirm that you qualify for this apartment by linking your bank account.</SpacedH3>
                 <img src={safeImage} alt="vault"/>
                 <div className={bodyContainer}>
@@ -92,7 +92,8 @@ export class ConnectBankPage extends React.Component {
                 <ActionButton onClick={this.openFinicityIframe} marginBottom="20px">
                     Link Bank Account
                 </ActionButton>
-                <Link to={ROUTES.MANUAL_INCOME_ENTRY} className={linkRoot}>Don't Want to Link?</Link>
+                {/* add this back when we add manual income entry pages
+                <Link to={ROUTES.MANUAL_INCOME_ENTRY} className={linkRoot}>Don't Want to Link?</Link> */}
             </Fragment>
         );
     }
