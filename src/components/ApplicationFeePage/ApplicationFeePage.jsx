7f66ba2bea0b79494882a30a478421c6eddc88d8
pawlkris <paul.kristapovich@nestio.com>
2019-07-03 10:55:04 -0400

    ugly working stripe payments

--- src/components/ApplicationFeePage/ApplicationFeePage.jsx
+++ src/components/ApplicationFeePage/ApplicationFeePage.jsx
@@ -3,13 +3,15 @@ import { connect } from 'react-redux';
 import styled from '@emotion/styled';
 import { css } from 'emotion';
 import { Link } from 'react-router-dom';
-import { Elements } from 'react-stripe-elements';
 
 
+import { CardNumberElement, CardExpiryElement, CardCVCElement, injectStripe, Elements } from  'react-stripe-elements';
 import padlockImage from 'assets/images/connect-bank/padlock.png';
 import creditCardImage from 'assets/images/credit-card.png';
-import { H1, H3 } from 'assets/styles';
-import InjectedPaymentForm from './PaymentForm';
+import ActionButton from 'components/common/ActionButton/ActionButton';
+import { H1, H3, formContent } from 'assets/styles';
+import API from 'app/api';
+// import PaymentForm from './PaymentForm';
 
 const SpacedH3 = styled(H3)`
     margin: 15px 10% 30px 10%;
@@ -51,8 +53,8 @@ export class ApplicationFeePage extends React.Component {
                     </div>
                 </div>
                 <Elements>
-                    <InjectedPaymentForm/>
-                </Elements>
+                    <PaymentForm></PaymentForm>
+                </Elements> 
             </Fragment>
         );
     }
@@ -64,3 +66,37 @@ const mapStateToProps = state => ({
 });
 
 export default connect(mapStateToProps, null)(ApplicationFeePage);
+
+
+class _PaymentForm extends React.Component {
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.stripe.createToken({type: 'card', name: 'client card'}).then( res => {
+            debugger;
+            if (res.token) {
+                const data = {token: res.token.id};
+                API.stripePayment(data);
+            } else {
+                // some error
+            }
+        }).catch( res => {
+            debugger;
+            // omg more errors!
+        });
+    }
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <div className={formContent}>
+                    <CardNumberElement></CardNumberElement>
+                    <CardExpiryElement></CardExpiryElement>
+                    <CardCVCElement></CardCVCElement>
+                    <ActionButton>skipity bee-bop, jazz</ActionButton>
+                </div>
+            </form>
+
+        )
+    }
+}
+
+const PaymentForm = injectStripe(_PaymentForm);
--- src/components/ApplicationFeePage/PaymentForm.jsx
+++ src/components/ApplicationFeePage/PaymentForm.jsx
@@ -15,9 +15,10 @@ export class PaymentForm extends React.Component {
     handleSubmit = (e) => {
         e.preventDefault();
         this.props.stripe.createToken({type: 'card', name: 'client card'}).then( res => {
-            if (res.token) {
-                API.stripePayment(res);
-            } else {
+                if (res.token) {
+                    const data = {token: res.token.id};
+                    API.stripePayment(data);
+                } else {
                 // some error
             }
         }).catch( res => {
@@ -50,53 +51,53 @@ export default injectStripe(PaymentForm);
 class StripeElementWrapper extends PureComponent {
   
     static propTypes = {
-      component: PropTypes.func.isRequired,
-      label: PropTypes.string.isRequired,
+        component: PropTypes.func.isRequired,
+        label: PropTypes.string.isRequired,
     }
   
     state = {
-      focused: false,
-      empty: true,
-      error: false,
+          focused: false,
+          empty: true,
+          error: false,
     }
   
     handleBlur = () => {
-      this.setState({ focused: false })
+        this.setState({ focused: false })
     }
   
     handleFocus = () => {
-      this.setState({ focused: true })
+        this.setState({ focused: true })
     }
   
     handleChange = changeObj => {
-      this.setState({
-        error: changeObj.error,
-        empty: changeObj.empty,
-      })
+        this.setState({
+          error: changeObj.error,
+          empty: changeObj.empty,
+        })
     }
   
     render() {
-      const { component, label } = this.props
-      const { focused, empty, error } = this.state
+        const { component, label } = this.props
+        const { focused, empty, error } = this.state
   
-      return (
-        <div>
-          <FormControl fullWidth margin="normal">
-            <InputLabel
-              focused={focused}
-              shrink={focused || !empty}
-              error={!!error}>
-              {label}
-            </InputLabel>
-            <Input
-              fullWidth
-              inputComponent={StripeInput}
-              onFocus={this.handleFocus}
-              onBlur={this.handleBlur}
-              onChange={this.handleChange}
-              inputProps={{ component }}
-            />
-          </FormControl>
+        return (
+            <div>
+                <FormControl fullWidth margin="normal">
+                    <InputLabel
+                        focused={focused}
+                        shrink={focused || !empty}
+                        error={!!error}>
+                        {label}
+                    </InputLabel>
+                    <Input
+                        fullWidth
+                        inputComponent={StripeInput}
+                        onFocus={this.handleFocus}
+                        onBlur={this.handleBlur}
+                        onChange={this.handleChange}
+                        inputProps={{ component }}
+                    />
+                </FormControl>
           {error && <ErrorDetail>{error.message}</ErrorDetail>}
         </div>
       )
@@ -105,57 +106,56 @@ class StripeElementWrapper extends PureComponent {
   
 
   class _StripeInput extends PureComponent {
-    
   
     static propTypes = {
-      classes: PropTypes.object.isRequired,
-      theme: PropTypes.object.isRequired,
-      component: PropTypes.func.isRequired,
-      onBlur: PropTypes.func,
-      onFocus: PropTypes.func,
-      onChange: PropTypes.func,
+        classes: PropTypes.object.isRequired,
+        theme: PropTypes.object.isRequired,
+        component: PropTypes.func.isRequired,
+        onBlur: PropTypes.func,
+        onFocus: PropTypes.func,
+        onChange: PropTypes.func,
     }
   
     static defaultProps = {
-      onFocus: () => {},
-      onBlur: () => {},
-      onChange: () => {},
-      classes: {
-        root: {
-          width: '100%',
-          padding: '6px 0 7px',
-          cursor: 'text',
-        },  
-      }
+        onFocus: () => {},
+        onBlur: () => {},
+        onChange: () => {},
+        classes: {
+            root: {
+            width: '100%',
+            padding: '6px 0 7px',
+            cursor: 'text',
+            },  
+        }
     }
   
     render() {
-      const {
-        classes: c,
-        theme,
-        component: Component,
-        onFocus,
-        onBlur,
-        onChange,
-      } = this.props
-      debugger;
-      return (
-        <Component
-          className={c.root}
-          onFocus={onFocus}
-          onBlur={onBlur}
-          onChange={onChange}
-          placeholder=""
-          style={{
-            base: {
-              fontSize: `${theme.typography.fontSize}px`,
-              fontFamily: theme.typography.fontFamily,
-              color: '#000000de',
-            },
-          }}
-        />
-      )
+        const {
+            classes: c,
+            theme,
+            component: Component,
+            onFocus,
+            onBlur,
+            onChange,
+        } = this.props
+
+        return (
+            <Component
+                className={c.root}
+                onFocus={onFocus}
+                onBlur={onBlur}
+                onChange={onChange}
+                placeholder=""
+                style={{
+                    base: {
+                    fontSize: `${theme.typography.fontSize}px`,
+                    fontFamily: theme.typography.fontFamily,
+                    color: '#000000de',
+                    },
+                }}
+            />
+        )
     }
-  }
+}
   
   const StripeInput = withTheme(_StripeInput);
