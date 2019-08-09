30cde44c352f97bccaa3d193ca52a8a39c36ed46
scott dikowitz <dikowitz@gmail.com>
2019-08-09 10:43:46 -0400

    fix label

--- src/components/LeaseTermsPage.jsx
+++ src/components/LeaseTermsPage.jsx
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
+import FormControl from '@material-ui/core/FormControl';
 import Grid from '@material-ui/core/Grid';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormHelperText from '@material-ui/core/FormHelperText';
@@ -125,21 +126,23 @@ export class LeaseTermsPage extends React.Component {
                                         />
                                     </Grid>
                                     <Grid item xs={12}>
-                                        <InputLabel htmlFor="lease-term">Lease Term</InputLabel>
-                                        <Select
-                                            fullWidth
-                                            value={values.lease_term}
-                                            onChange={handleChange}
-                                            inputProps={{
-                                                name: 'lease_term',
-                                                id: 'lease-term',
-                                            }}
-                                        >
-                                            {this.props.config.lease_term_options.map(choice => (
-                                                <MenuItem key={choice} value={choice}>{choice} Months</MenuItem>
-                                            ))}
-                                        </Select>
-                                        <FormHelperText>{values.move_in_date && values.lease_term && `Ends ${offsetDate(values.move_in_date, values.lease_term)}`}</FormHelperText>
+                                        <FormControl fullWidth>
+                                            <InputLabel htmlFor="lease-term">Lease Term</InputLabel>
+                                            <Select
+                                                fullWidth
+                                                value={values.lease_term}
+                                                onChange={handleChange}
+                                                inputProps={{
+                                                    name: 'lease_term',
+                                                    id: 'lease-term',
+                                                }}
+                                            >
+                                                {this.props.config.lease_term_options.map(choice => (
+                                                    <MenuItem key={choice} value={choice}>{choice} Months</MenuItem>
+                                                ))}
+                                            </Select>
+                                            <FormHelperText>{values.move_in_date && values.lease_term && `Ends ${offsetDate(values.move_in_date, values.lease_term)}`}</FormHelperText>
+                                        </FormControl>
                                     </Grid>
                                 </Grid>
                             </div>
