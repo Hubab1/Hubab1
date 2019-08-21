3e80c2ee4b50c4d905bdc8836c582963ea029554
pawlkris <paul.kristapovich@nestio.com>
2019-08-21 16:32:39 -0400

    add tests
    ;

---
+++ src/components/fees-deposits/PaymentTerms.spec.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import ActionButton from 'components/common/ActionButton/ActionButton';
+import { LinkButton } from 'assets/styles';
+import PaymentTerms from './PaymentTerms';
+
+let defaultProps;
+beforeEach(() => {
+    defaultProps = {
+        handleClickBack: jest.fn(),
+        goToPayment: jest.fn(),
+    }
+})
+
+it('calls handleClickBack when Go Back is clicked', () => {
+    const wrapper = shallow( <PaymentTerms {...defaultProps} />);
+
+    wrapper.find('LinkButton').simulate('click');
+    expect(defaultProps.handleClickBack).toHaveBeenCalled();
+})
+
+it('calls goToPayment when Go Back is clicked', () => {
+    const wrapper = shallow( <PaymentTerms {...defaultProps} />);
+
+    wrapper.find('ActionButton').simulate('click');
+    expect(defaultProps.goToPayment).toHaveBeenCalled();
+})
