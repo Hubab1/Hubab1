12887edc073841feec7710df7518c76f7b4bac64
Vishal Patel <vpatel85@gmail.com>
2019-10-10 15:42:21 -0400

    feat(src/index/.env):add google tag manager

--- src/index.js
+++ src/index.js
@@ -5,6 +5,16 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from 'reducers/store';
 import App from 'app/App';
+import TagManager from 'react-gtm-module';
+
+const GTM_AUTH = process.env.GTM_AUTH;
+
+const tagManagerArgs = {
+    gtmId: 'GTM-W8J3S8J',
+    auth: GTM_AUTH
+};
+
+TagManager.initialize(tagManagerArgs);
 
 ReactDOM.render((
     <Provider store={store}>
