5a539fcb76145ae8f93efcc3bc226e3223f2aacb
pawlkris <paul.kristapovich@nestio.com>
2019-12-04 16:12:25 -0500

    refactor ChangePasswordForm and reset password on account detail

--- src/components/account/AccountPage.spec.js
+++ src/components/account/AccountPage.spec.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import { AccountPage } from './AccountPage';
+import { LinkButton } from 'assets/styles';
+import ChangePasswordForm from 'components/common/ChangePasswordForm';
 import AccountForm from 'components/common/AccountForm';
 import VerifyAccount from 'components/account/VerifyAccount';
 import mockApplicant from 'reducers/applicant-mock.json';
@@ -29,4 +31,13 @@ it('renders AccountForm when verified', () => {
 
     expect(wrapper.find(VerifyAccount).exists()).not.toBeTruthy();
     expect(wrapper.find(AccountForm).exists()).toBeTruthy();
+});
+
+it('renders ChangePasswordForm when password has been verified and Change Password button is clicked', () => {
+    const wrapper = shallow(<AccountPage {...defaultProps}/>);
+    wrapper.setState({verified:true});
+    expect(wrapper.find(ChangePasswordForm).exists()).not.toBeTruthy();
+
+    wrapper.find(LinkButton).simulate('click');
+    expect(wrapper.find(ChangePasswordForm).exists()).toBeTruthy();
 });
