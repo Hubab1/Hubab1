002e7c330d269a61d8a158b57409ddee9aa1a484
pawlkris <paul.kristapovich@nestio.com>
2020-01-15 14:29:56 -0500

    display co_applicants

--- src/components/profile/options/RenterProfileListItem.jsx
+++ src/components/profile/options/RenterProfileListItem.jsx
@@ -1,14 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
 import Info from '@material-ui/icons/Info';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 
-import { infoIconRoot } from 'assets/styles';
+import pluralize from 'pluralize';
+
+import { infoIconRoot, P } from 'assets/styles';
 import SimplePopover from 'components/common/SimplePopover';
 
 
-import { contentContainer, label, prefix, buttonRoot, paperRoot, renterProfileListItemContainer } from './styles';
+import { applicationStatus, buttonRoot, contentContainer, existingItemRow, label, prefix, paperRoot, renterProfileListItemContainer } from './styles';
+
+function ExistingRoommate({item}) {
+    const statusColor = !!item.status ? '#DB5963' : '#FAC700';
+    return <div className={existingItemRow}>
+        <div>{`${item.first_name} ${item.last_name}`}</div>
+        <div>
+            <span className={applicationStatus}>Application Status:</span>
+            <br/>
+            <P color={statusColor}>{!!item.status ? item.status : 'Not Started'}</P>
+        </div>
+    </div>
+}
+
+function ExistingItems({existingItems, nameLabel}) {
+    const [showItems, setShowItems] = useState(false);
+    const toggleLabel = `${existingItems.length} ${pluralize(nameLabel, existingItems.length)}`;
+    const toggleIcon = !!showItems ? <ExpandLessIcon/> : <ExpandMoreIcon/>
+    const existingItemComponentMap = {
+        'Roommates': ExistingRoommate,
+    }
+    const ExistingItemComponent = existingItemComponentMap[nameLabel];
+    return (
+        <div className="existingItemsContainer">
+            <div onClick={() => setShowItems(!showItems)} className={existingItemRow}>{toggleLabel}&nbsp;Added&nbsp;{toggleIcon}</div>
+            {showItems && existingItems.map(item => <ExistingItemComponent item={item} key={item.id}/>)}
+        </div>
+    )
+}
 
 function RenterProfileListItem (props) {
     return (
@@ -22,6 +54,7 @@ function RenterProfileListItem (props) {
                         </SimplePopover>}
                     </div>
                 </div>
+                {!!props.existingItems && <ExistingItems nameLabel={props.nameLabel} existingItems={props.existingItems}/>}
                 <Link to={props.route}>
                     <Button
                         variant="outlined"
--- src/components/profile/options/RenterProfileOptions.jsx
+++ src/components/profile/options/RenterProfileOptions.jsx
@@ -24,43 +24,48 @@ const SpacedH3 = styled(H3)`
     margin-bottom: 30px;
 `
 
-const optionConfig = {
-    co_applicants: {
-        prefix: <img alt="coapplicants" src={coapplicants}></img>,
-        name: 'co_applicants',
-        label: 'I\'ll be living with roommates',
-        buttonLabel: 'Invite a roommate',
-        route: ROUTES.CO_APPLICANTS
-    },
-    pets: {
-        prefix: <img alt="dog" src={doggie}></img>,
-        name: 'pets',
-        label: 'I\'ll be living with pets',
-        buttonLabel: 'Add a pet',
-        route: ROUTES.PETS
-    },
-    guarantor: {
-        prefix: <img alt="coins" src={guarantor}></img>,
-        name: 'guarantor',
-        label: 'I\'ll need a guarantor',
-        buttonLabel: 'Invite a guarantor',
-        route: ROUTES.GUARANTOR,
-        tip: 'This is a person that agrees to be legally responsible for the apartment, its condition, and the money owed for rent if you are unable to pay.'
-    },
-    parking: {
-        prefix: '🚗',
-        name: 'parking',
-        label: 'I\'ll need parking',
-        buttonLabel: 'Add parking',
-        route: ROUTES.PARKING
-    },
-    storage: {
-        prefix: '🛍️',
-        name: 'storage',
-        label: 'I\'ll need storage',
-        buttonLabel: 'Add storage',
-        route: ROUTES.STORAGE
-    }
+const getOptionConfig = (option, profile) => {
+    const optionConfigObj = {
+        co_applicants: {
+            prefix: <img alt="coapplicants" src={coapplicants}></img>,
+            name: 'co_applicants',
+            nameLabel: 'Roommates',
+            label: 'I\'ll be living with roommates',
+            buttonLabel: (!!profile.co_applicants.length ? 'Invite another roommate' : 'Invite a roommate'),
+            route: ROUTES.CO_APPLICANTS,
+            existingItems: profile.co_applicants
+        },
+        pets: {
+            prefix: <img alt="dog" src={doggie}></img>,
+            name: 'pets',
+            label: 'I\'ll be living with pets',
+            buttonLabel: 'Add a pet',
+            route: ROUTES.PETS
+        },
+        guarantor: {
+            prefix: <img alt="coins" src={guarantor}></img>,
+            name: 'guarantor',
+            label: 'I\'ll need a guarantor',
+            buttonLabel: 'Invite a guarantor',
+            route: ROUTES.GUARANTOR,
+            tip: 'This is a person that agrees to be legally responsible for the apartment, its condition, and the money owed for rent if you are unable to pay.'
+        },
+        parking: {
+            prefix: '🚗',
+            name: 'parking',
+            label: 'I\'ll need parking',
+            buttonLabel: 'Add parking',
+            route: ROUTES.PARKING
+        },
+        storage: {
+            prefix: '🛍️',
+            name: 'storage',
+            label: 'I\'ll need storage',
+            buttonLabel: 'Add storage',
+            route: ROUTES.STORAGE
+        }
+    };
+    return optionConfigObj[option];
 }
 
 export class RentalProfileOptions extends React.Component {
@@ -86,7 +91,7 @@ export class RentalProfileOptions extends React.Component {
                     {sortedRentalOptions.map(option => (
                         <RenterProfileListItem
                             key={option}
-                            {...optionConfig[option]}
+                            {...getOptionConfig(option, this.props.profile)}
                         />
                     ))}
                 </div>
