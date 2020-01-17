c38c6af461eb13418b97eceda65415239984e3d1
Scott Dikowitz <dikowitz@gmail.com>
2020-01-17 10:26:41 -0500

    remove config map

--- src/components/profile/options/RenterProfileOptions.jsx
+++ src/components/profile/options/RenterProfileOptions.jsx
@@ -4,7 +4,7 @@ import styled from '@emotion/styled';
 import { connect } from 'react-redux';
 
 import ActionButton from 'components/common/ActionButton/ActionButton';
-import { ROUTES, SORTED_CONFIG_OPTIONS, RENTER_PROFILE_IDENTIFIER } from 'app/constants';
+import { ROUTES, RENTER_PROFILE_IDENTIFIER } from 'app/constants';
 import { updateRenterProfile, pageComplete } from 'reducers/renter-profile';
 import ExistingRoommate from './ExistingRoommate';
 import RenterProfileListItem from './RenterProfileListItem';
@@ -25,51 +25,6 @@ const SpacedH3 = styled(H3)`
     margin-bottom: 30px;
 `
 
-const getOptionConfig = (option, profile) => {
-    const optionConfigObj = {
-        co_applicants: {
-            prefix: <img alt="coapplicants" src={coapplicants}></img>,
-            name: 'co_applicants',
-            label: 'I\'ll be living with roommates',
-            buttonLabel: (!!profile.co_applicants.length ? 'Invite another roommate' : 'Invite a roommate'),
-            route: ROUTES.CO_APPLICANTS,
-            existingItems: profile.co_applicants,
-            existingItemsLabel: 'Roommates',
-            existingItemComponent: ExistingRoommate
-        },
-        pets: {
-            prefix: <img alt="dog" src={doggie}></img>,
-            name: 'pets',
-            label: 'I\'ll be living with pets',
-            buttonLabel: 'Add a pet',
-            route: ROUTES.PETS
-        },
-        guarantor: {
-            prefix: <img alt="coins" src={guarantor}></img>,
-            name: 'guarantor',
-            label: 'I\'ll need a guarantor',
-            buttonLabel: 'Invite a guarantor',
-            route: ROUTES.GUARANTOR,
-            tip: 'This is a person that agrees to be legally responsible for the apartment, its condition, and the money owed for rent if you are unable to pay.'
-        },
-        parking: {
-            prefix: '🚗',
-            name: 'parking',
-            label: 'I\'ll need parking',
-            buttonLabel: 'Add parking',
-            route: ROUTES.PARKING
-        },
-        storage: {
-            prefix: '🛍️',
-            name: 'storage',
-            label: 'I\'ll need storage',
-            buttonLabel: 'Add storage',
-            route: ROUTES.STORAGE
-        }
-    };
-    return optionConfigObj[option];
-}
-
 export class RentalProfileOptions extends React.Component {
     state = { submitting: false }
     onContinue = async () => {
@@ -82,26 +37,71 @@ export class RentalProfileOptions extends React.Component {
         this.props._nextRoute();
     }
 
-
+    get configurableRentalOptions () {
+        // for now we assume all require co_appplicants and guarantor... might want to do something smarter with RentalOptions in the future, but okie for now
+        const activeRentalOptions = new Set(['co_applicants', 'guarantor']);
+        Object.keys(this.props.config.options).forEach(key => activeRentalOptions.add(key.toLowerCase()));
+        return activeRentalOptions;
+    }
 
     render () {
-        if (this.props.config == null || this.props.profile == null) return null;
-        const unsortedOptionsSet = new Set(Object.keys(this.props.config.options).map(key => key.toLowerCase()));
-        // for now we assume all require co_appplicants and guarantor... might want to do something smarter with RentalOptions in the future, but okie for now
-        const sortedRentalOptions = ['co_applicants', 'guarantor'].concat(SORTED_CONFIG_OPTIONS.filter(option => unsortedOptionsSet.has(option))
-        );
+        if (this.props.profile == null) return null;
+        const options = this.configurableRentalOptions;
 
         return (
             <Fragment>
                 <SkinnyH1>Let's Set Up Your Rental Profile</SkinnyH1>
                 <SpacedH3>Complete the sections that apply to you and skip the ones that don't.</SpacedH3>
                 <div>
-                    {sortedRentalOptions.map(option => (
+                    {options.has('co_applicants') &&
                         <RenterProfileListItem
-                            key={option}
-                            {...getOptionConfig(option, this.props.profile)}
+                            prefix={<img alt="coapplicants" src={coapplicants}></img>}
+                            name="co_applicants"
+                            label="I'll be living with roommates"
+                            buttonLabel={!!this.props.profile.co_applicants.length ? 'Invite another roommate' : 'Invite a roommate'}
+                            route={ROUTES.CO_APPLICANTS}
+                            existingItems={this.props.profile.co_applicants}
+                            existingItemsLabel={'Roommates'}
+                            existingItemComponent={ExistingRoommate}
                         />
-                    ))}
+                    }
+                    {options.has('guarantor') &&
+                        <RenterProfileListItem
+                            prefix={<img alt="coins" src={guarantor}></img>}
+                            name="guarantor"
+                            label="I'll need a guarantor"
+                            buttonLabel="Invite a guarantor"
+                            route={ROUTES.GUARANTOR}
+                            tip="This is a person that agrees to be legally responsible for the apartment, its condition, and the money owed for rent if you are unable to pay."
+                        />
+                    }
+                    {options.has('pets') &&
+                        <RenterProfileListItem
+                            prefix={<img alt="dog" src={doggie}></img>}
+                            name="pets"
+                            label="I'll be living with pets"
+                            buttonLabel="Add a pet"
+                            route={ROUTES.PETS}
+                        />
+                    }
+                    {options.has('parking') &&
+                        <RenterProfileListItem
+                            prefix="🚗"
+                            name="parking"
+                            label="I'll need parking"
+                            buttonLabel="Add parking"
+                            route={ROUTES.PARKING}
+                        />
+                    }
+                    {options.has('storage') &&
+                        <RenterProfileListItem
+                            prefix="🛍️"
+                            name="storage"
+                            label="I'll need storage"
+                            buttonLabel="Add storage"
+                            route={ROUTES.STORAGE}
+                        />
+                    }
                 </div>
                 <ActionButton disabled={this.state.submitting} onClick={this.onContinue} marginTop={60} marginBottom={27}>Continue</ActionButton>
                 <BackLink to={this.props._prev}/>
