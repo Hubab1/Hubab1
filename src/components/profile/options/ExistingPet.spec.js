a0086e9497df64144b24c1920ef589a2ecb10952
pawlkris <paul.kristapovich@nestio.com>
2020-01-19 14:21:55 -0500

    fix tests

--- src/components/profile/options/ExistingPet.spec.js
+++ src/components/profile/options/ExistingPet.spec.js
@@ -5,17 +5,13 @@ import ExistingPet from 'components/profile/options/ExistingPet';
 
 it('renders pet info correctly', () => {
     const petItem = {
-        leasing_context: {
-            pets: [{
                 key:"first-pet",
                 name:"stuart",
                 weight:"12",
                 breed:"chiwowzerz",
                 pet_type:"Dog",
                 service_animal:"true",
-            }]
-        }
-    }
+    };
     const wrapper = shallow(<ExistingPet item={petItem}/>);
     expect(wrapper.text()).toEqual('Dog - Service Animalstuart, chiwowzerz, 12lbs');
 });
