f6e661b14be059709f77f85b74713aa9965c7abc
Scott Dikowitz <dikowitz@gmail.com>
2020-04-02 15:59:43 -0400

    hotfix checkbox padding

--- src/components/common/Checkbox.jsx
+++ src/components/common/Checkbox.jsx
@@ -3,8 +3,9 @@ import Checkbox from '@material-ui/core/Checkbox';
 import Grid from '@material-ui/core/Grid';
 import { css } from 'emotion';
 
-const noPaddingTop = css`
+const checkboxRoot = css`
     padding-top: 0 !important;
+    padding-left: 0 !important;
 `
 const checkboxText = css`
     font-size: 12px;
@@ -17,26 +18,23 @@ const textLeft = css`
 
 const gridContainer = css`
     padding: 20px 0 20px 0;
+    text-align: left;
+    display: flex;
+    align-items: flex-start;
 `
 
 export default function ({name, value, checked, onChange, error, label}) {
     return (
         <div className={gridContainer}>
-            <Grid container spacing={0} alignItems="flex-start">
-                <Grid item xs={2}>
-                    <Checkbox
-                        name={name}
-                        onChange={onChange}
-                        checked={checked}
-                        value={value}
-                        error={error}
-                        classes={{ root: noPaddingTop }}
-                    />
-                </Grid>
-                <Grid classes={{root: textLeft}} item xs={10}>
-                    <span className={checkboxText}>{label}</span>
-                </Grid>
-            </Grid>
+            <Checkbox
+                name={name}
+                onChange={onChange}
+                checked={checked}
+                value={value}
+                error={error}
+                classes={{ root: checkboxRoot }}
+            />
+            <span className={checkboxText}>{label}</span>
         </div>
     )
 }
