aadd8ec91b83b4e3b5f16ebf5be4a55ae9979ed1
Matt Goldeck <matt@goldeck.net>
2020-06-22 10:32:54 -0400

    NESTIO-14689 - slightly nicer testing

--- src/components/NavStepper.spec.js
+++ src/components/NavStepper.spec.js
@@ -103,7 +103,7 @@ describe('Application submitted state', function() {
 });
 
 describe('Unit unavailable state', function() {
-    it("renders an unit unavailable message", function() {
+    it("Renders a unit unavailable message", function() {
         const defaultProps = {
             currentRoute: ROUTES.UNIT_UNAVAILABLE,
             applicantStillFinishingApplication: true,
@@ -118,6 +118,26 @@ describe('Unit unavailable state', function() {
         const appCompletedMsg =  wrapper.find('.unitUnavailableMsg');
         expect(appCompletedMsg.text()).toContain('We\'ve placed your application on hold for now, since the apartment you were interested in is no longer available. Please call us at 123-456-7891 so we can discuss some other options.');
         expect(wrapper.find('#viewProgressButton').text()).toContain('View Progress');
+    })
+
+    it("View Progress when clicked takes to initialPage set", function() {
+        const defaultProps = {
+            currentRoute: ROUTES.UNIT_UNAVAILABLE,
+            applicantStillFinishingApplication: true,
+            navRoutes: [],
+            config: {
+                community: {
+                    contact_phone: '123-456-7891'
+                }
+            },
+            history: {
+                push: jest.fn(),
+            },
+            initialPage: ROUTES.UNIT_UNAVAILABLE,
+        };
+        const wrapper = shallow(<VerticalLinearStepper {...defaultProps} />);
+        wrapper.find('#viewProgressButton').simulate('click');
+        expect(defaultProps.history.push).toHaveBeenCalledWith('/unit-unavailable');
     })
 });
 
