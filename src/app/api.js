49e5dafff00b8a50e6a45ad992886a9781c8fb54
Chakib <ljazouli.c@gmail.com>
2020-10-13 07:42:21 -0400

    NESTIO-17239-Lease-Terms-From-Yardi-FE

--- src/app/api.js
+++ src/app/api.js
@@ -7,16 +7,17 @@ export function chuck(path) {
     return `${CHUCK_BASE_URL}/api/onlineleasing${path}`;
 }
 
-const getWithHeaders = (url) => fetch(url, {
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-    }
-}).then(res => res.json());
+const getWithHeaders = (url) =>
+    fetch(url, {
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+        },
+    }).then((res) => res.json());
 
 const CHUCK_BASE_LEASE_SETTINGS = (communityId) => `${CHUCK_BASE_URL}/api/onlineleasing/communities/${communityId}/`;
-const CHUCK_PERSONALIZED_LEASE_SETTINGS =
-    (communityId, hash) => `${CHUCK_BASE_URL}/api/onlineleasing/communities/${communityId}/personalized/${hash}`;
+const CHUCK_PERSONALIZED_LEASE_SETTINGS = (communityId, hash) =>
+    `${CHUCK_BASE_URL}/api/onlineleasing/communities/${communityId}/personalized/${hash}`;
 
 const API = {};
 
@@ -36,168 +37,171 @@ API.postPageComplete = (page) => {
     return fetch(chuck(`/page-complete/`), {
         method: 'POST',
         headers: {
-            Authorization: `Token ${auth.getToken()}`
+            Authorization: `Token ${auth.getToken()}`,
         },
-        body: JSON.stringify({ page })
-    }).then(res => res.json());
+        body: JSON.stringify({ page }),
+    }).then((res) => res.json());
 };
 
 API.fetchAvailableUnits = () => {
-    if (MOCKY) return Promise.resolve([
-        {id: 1, unit_number: '1B', price: 1540.50},
-        {id: 2, unit_number: '1C', price: 1999.99},
-        {id: 3, unit_number: '1D', price: 5000.00},
-        {id: 4, unit_number: '1E', price: 2450.99},
-        {id: 5, unit_number: '2E', price: 2450.99},
-        {id: 6, unit_number: '2A', price: 1540.50},
-        {id: 7, unit_number: '2B', price: 1999.99},
-        {id: 8, unit_number: '2C', price: 5000.00},
-        {id: 9, unit_number: '2D', price: 2450.99},
-        {id: 10, unit_number: '2E', price: 2450.99},
-    ]);
+    if (MOCKY)
+        return Promise.resolve([
+            { id: 1, unit_number: '1B', price: 1540.5 },
+            { id: 2, unit_number: '1C', price: 1999.99 },
+            { id: 3, unit_number: '1D', price: 5000.0 },
+            { id: 4, unit_number: '1E', price: 2450.99 },
+            { id: 5, unit_number: '2E', price: 2450.99 },
+            { id: 6, unit_number: '2A', price: 1540.5 },
+            { id: 7, unit_number: '2B', price: 1999.99 },
+            { id: 8, unit_number: '2C', price: 5000.0 },
+            { id: 9, unit_number: '2D', price: 2450.99 },
+            { id: 10, unit_number: '2E', price: 2450.99 },
+        ]);
     return fetch(chuck('/available-units/'), {
         method: 'GET',
         headers: {
-            Authorization: `Token ${auth.getToken()}`
-        }
-    }).then(res => res.json());
+            Authorization: `Token ${auth.getToken()}`,
+        },
+    }).then((res) => res.json());
 };
 
 API.patchApplication = (data) => {
     return fetch(chuck('/application/'), {
         method: 'PATCH',
         headers: {
-            Authorization: `Token ${auth.getToken()}`
+            Authorization: `Token ${auth.getToken()}`,
         },
-        body: JSON.stringify(data)
-    }).then(res => res.json());
+        body: JSON.stringify(data),
+    }).then((res) => res.json());
 };
 
 API.putApplicant = (data) => {
     return fetch(chuck('/applicant/'), {
         method: 'PUT',
         headers: {
-            Authorization: `Token ${auth.getToken()}`
+            Authorization: `Token ${auth.getToken()}`,
         },
-        body: JSON.stringify(data)
-    }).then(res => res.json());
+        body: JSON.stringify(data),
+    }).then((res) => res.json());
 };
 
 API.fetchRenterProfile = () => {
     return fetch(chuck('/application/'), {
         method: 'GET',
         headers: {
-            Authorization: `Token ${auth.getToken()}`
-        }
-    }).then(res => res.json());
+            Authorization: `Token ${auth.getToken()}`,
+        },
+    }).then((res) => res.json());
 };
 
 API.fetchApplicant = () => {
     return fetch(chuck('/applicant/'), {
         method: 'GET',
         headers: {
-            Authorization: `Token ${auth.getToken()}`
-        }
-    }).then(res => res.json());
+            Authorization: `Token ${auth.getToken()}`,
+        },
+    }).then((res) => res.json());
 };
 
 API.login = (email, password, communityId) => {
     return fetch(chuck(`/communities/${communityId}/login/`), {
         method: 'POST',
-        body: JSON.stringify({ email, password })
-    }).then(res => res.json());
+        body: JSON.stringify({ email, password }),
+    }).then((res) => res.json());
 };
 
 API.register = (data, leaseSettingsId, hash) => {
     let registerUrl = `/communities/${leaseSettingsId}/register/`;
-    if (hash) { registerUrl = registerUrl.concat(`?v=${hash}`); }
+    if (hash) {
+        registerUrl = registerUrl.concat(`?v=${hash}`);
+    }
     return fetch(chuck(registerUrl), {
         method: 'POST',
-        body: JSON.stringify(data)
-    }).then(res => res.json());
+        body: JSON.stringify(data),
+    }).then((res) => res.json());
 };
 
 API.acceptTerms = (data) => {
     return fetch(chuck('/terms-accepted/'), {
         method: 'POST',
         headers: {
-            Authorization: `Token ${auth.getToken()}`
+            Authorization: `Token ${auth.getToken()}`,
         },
-        body: JSON.stringify(data)
-    }).then(res => res.json());
+        body: JSON.stringify(data),
+    }).then((res) => res.json());
 };
 
 API.passwordResetRequest = (phone_number, lease_settings_id) => {
     return fetch(chuck('/password-reset/'), {
         method: 'POST',
-        body: JSON.stringify({ phone_number, lease_settings_id })
-    }).then(res => res.json());
+        body: JSON.stringify({ phone_number, lease_settings_id }),
+    }).then((res) => res.json());
 };
 
 API.passwordResetVerification = (phone_number, code, lease_settings_id) => {
     return fetch(chuck('/password-reset-verification/'), {
         method: 'POST',
-        body: JSON.stringify({ phone_number, code, lease_settings_id })
-    }).then(res => res.json());
+        body: JSON.stringify({ phone_number, code, lease_settings_id }),
+    }).then((res) => res.json());
 };
 
 API.passwordReset = (password, token) => {
     return fetch(chuck('/password-change/'), {
         method: 'PUT',
-        body: JSON.stringify({password}),
+        body: JSON.stringify({ password }),
         headers: {
             'Content-Type': 'application/json',
-            'AUTHORIZATION': `token ${token}`,
-        }
-    }).then(res => res.json());
+            AUTHORIZATION: `token ${token}`,
+        },
+    }).then((res) => res.json());
 };
 
 API.inviteGuarantor = (data) => {
     return fetch(chuck('/guarantors/'), {
         method: 'POST',
         headers: {
-            Authorization: `Token ${auth.getToken()}`
+            Authorization: `Token ${auth.getToken()}`,
         },
-        body: JSON.stringify(data)
-    }).then(res => res.json());
+        body: JSON.stringify(data),
+    }).then((res) => res.json());
 };
 
 API.createFinicityUrl = () => {
     return fetch(chuck('/generate-finicity-link/'), {
         headers: {
-            Authorization: `Token ${auth.getToken()}`
+            Authorization: `Token ${auth.getToken()}`,
         },
-    }).then(res => res.json());
+    }).then((res) => res.json());
 };
 
 API.generateFinicityReports = () => {
     return fetch(chuck('/generate-finicity-reports/'), {
         headers: {
-            Authorization: `Token ${auth.getToken()}`
+            Authorization: `Token ${auth.getToken()}`,
         },
-    }).then(res => res.json());
+    }).then((res) => res.json());
 };
 
 API.embeddedSigningUrl = (type) => {
     return fetch(chuck(`/embedded-signing-url/?document_type=${type}`), {
         headers: {
-            Authorization: `Token ${auth.getToken()}`
+            Authorization: `Token ${auth.getToken()}`,
         },
-    }).then(res => res.json());
+    }).then((res) => res.json());
 };
 
 API.leaseDocumentUrl = (type) => {
     return fetch(chuck(`/lease-document-url/?document_type=${type}`), {
         headers: {
-            Authorization: `Token ${auth.getToken()}`
+            Authorization: `Token ${auth.getToken()}`,
         },
-    }).then(res => res.json());
+    }).then((res) => res.json());
 };
 
 API.fetchFinicityReports = () => {
     return fetch(chuck('/fetch-finicity-reports/'), {
         headers: {
-            Authorization: `Token ${auth.getToken()}`
+            Authorization: `Token ${auth.getToken()}`,
         },
     });
 };
@@ -206,39 +210,38 @@ API.stripePayment = (data) => {
     return fetch(chuck('/payment/'), {
         method: 'POST',
         headers: {
-            Authorization: `Token ${auth.getToken()}`
+            Authorization: `Token ${auth.getToken()}`,
         },
-        body: JSON.stringify(data)
-    }).then(res => res.json());
+        body: JSON.stringify(data),
+    }).then((res) => res.json());
 };
 
 API.updateInvitee = (data, inviteeId) => {
     return fetch(chuck(`/invitees/${inviteeId}/`), {
         method: 'PUT',
         headers: {
-            Authorization: `Token ${auth.getToken()}`
+            Authorization: `Token ${auth.getToken()}`,
         },
-        body: JSON.stringify(data)
-    }).then(res => res.json());
+        body: JSON.stringify(data),
+    }).then((res) => res.json());
 };
 
-API.postPassthrough = data => (
+API.postPassthrough = (data) =>
     fetch(chuck('/passthrough/'), {
         method: 'POST',
         headers: {
-            Authorization: `Token ${auth.getToken()}`
+            Authorization: `Token ${auth.getToken()}`,
         },
-        body: JSON.stringify(data)
-    }).then(res => res.json())
-);
+        body: JSON.stringify(data),
+    }).then((res) => res.json());
 
 API.fetchPaymentOptions = () => {
     return fetch(chuck('/payment-options/'), {
         method: 'GET',
         headers: {
-            Authorization: `Token ${auth.getToken()}`
+            Authorization: `Token ${auth.getToken()}`,
         },
-    }).then(res => {
+    }).then((res) => {
         return res.json();
     });
 };
@@ -247,10 +250,10 @@ API.getCurrentFlatQuote = (data) => {
     return fetch(chuck('/payment-breakdown/'), {
         method: 'POST',
         headers: {
-            Authorization: `Token ${auth.getToken()}`
+            Authorization: `Token ${auth.getToken()}`,
         },
-        body: JSON.stringify(data)
-    }).then(res => {
+        body: JSON.stringify(data),
+    }).then((res) => {
         return res.json();
     });
 };
@@ -259,9 +262,9 @@ API.getAdverseActions = () => {
     return fetch(chuck('/applicant/adverse-action/'), {
         method: 'GET',
         headers: {
-            Authorization: `Token ${auth.getToken()}`
+            Authorization: `Token ${auth.getToken()}`,
         },
-    }).then(res => {
+    }).then((res) => {
         return res.json();
     });
 };
@@ -270,26 +273,26 @@ API.submitFinancialSource = (data) => {
     return fetch(chuck('/financial-sources/'), {
         method: 'POST',
         headers: {
-            'AUTHORIZATION': `Token ${auth.getToken()}`,
+            AUTHORIZATION: `Token ${auth.getToken()}`,
         },
-        body: data
+        body: data,
     });
 };
 
 API.getFinancialSources = () => {
     return fetch(chuck('/financial-sources/'), {
         headers: {
-            'Authorization': `Token ${auth.getToken()}`
-        }
+            Authorization: `Token ${auth.getToken()}`,
+        },
     });
 };
 
-API.getFinancialSource = id => {
+API.getFinancialSource = (id) => {
     return fetch(chuck(`/financial-sources/${id}/`), {
         headers: {
-            'Authorization': `Token ${auth.getToken()}`
-        }
-    }).then(res => {
+            Authorization: `Token ${auth.getToken()}`,
+        },
+    }).then((res) => {
         if (res.status === 200) {
             return res.json();
         }
@@ -302,9 +305,9 @@ API.updateFinancialSource = (id, body) => {
         method: 'PATCH',
         body,
         headers: {
-            'Authorization': `Token ${auth.getToken()}`
-        }
-    }).then(res => {
+            Authorization: `Token ${auth.getToken()}`,
+        },
+    }).then((res) => {
         if (res.status === 200) {
             return res.json();
         }
@@ -316,9 +319,9 @@ API.deleteFinancialSource = (id) => {
     return fetch(chuck(`/financial-sources/${id}/`), {
         method: 'DELETE',
         headers: {
-            'Authorization': `Token ${auth.getToken()}`
-        }
-    }).then(res => {
+            Authorization: `Token ${auth.getToken()}`,
+        },
+    }).then((res) => {
         if (res.status === 200) {
             return res.json();
         }
@@ -330,9 +333,9 @@ API.resetApplicantFinancials = () => {
     return fetch(chuck(`/financial-sources/`), {
         method: 'DELETE',
         headers: {
-            'Authorization': `Token ${auth.getToken()}`
-        }
-    }).then(res => {
+            Authorization: `Token ${auth.getToken()}`,
+        },
+    }).then((res) => {
         if (res.status >= 200 && res.status < 300) {
             return Promise.resolve();
         }
@@ -344,9 +347,9 @@ API.deletePerson = (id) => {
     return fetch(chuck(`/person/${id}/`), {
         method: 'DELETE',
         headers: {
-            'Authorization': `Token ${auth.getToken()}`
-        }
-    }).then(res => {
+            Authorization: `Token ${auth.getToken()}`,
+        },
+    }).then((res) => {
         if (res.status >= 200 && res.status < 300) {
             return;
         }
@@ -358,14 +361,28 @@ API.deleteInvitee = (id) => {
     return fetch(chuck(`/invitees/${id}/`), {
         method: 'DELETE',
         headers: {
-            'Authorization': `Token ${auth.getToken()}`
-        }
-    }).then(res => {
+            Authorization: `Token ${auth.getToken()}`,
+        },
+    }).then((res) => {
         if (res.status >= 200 && res.status < 300) {
             return;
         }
         throw new Error();
     });
+};
+
+API.fetchAvailableLeaseTerms = (data) => {
+    if (MOCKY)
+        return Promise.resolve({
+            lease_terms: [6, 12, 24],
+        });
+    return fetch(chuck(`/available-lease-terms/`), {
+        method: 'POST',
+        headers: {
+            Authorization: `Token ${auth.getToken()}`,
+        },
+        body: JSON.stringify(data),
+    }).then((res) => res.json());
 };
 
 export default API;
