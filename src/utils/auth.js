8d8ce4fb18789798c6ac8e80a2c0024c97bda465
JimVercoelen <jimvercoelen@gmail.com>
2020-10-26 15:51:39 +0100

    ran pretty quick

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -6,26 +6,28 @@ import API, { MOCKY } from 'app/api';
 // token auth service loosely based on the authentication service exemplified here: https://medium.appbase.io/how-to-implement-authentication-for-your-react-app-cf09eef3bb0b
 class Auth {
     register = (data, leaseSettingsId, clientId) => {
-        return API.register(data, leaseSettingsId, clientId).then((res)=>{
-            if (res.errors) {
-                return Promise.reject({errors: res.errors});
-            }
-            return Promise.resolve({token: res.token});
-        }).catch((e) => {
-            const error = get(e, 'errors.error._schema[0]');
-            const errorMsg = error || 'There was a problem creating your account.';
+        return API.register(data, leaseSettingsId, clientId)
+            .then((res) => {
+                if (res.errors) {
+                    return Promise.reject({ errors: res.errors });
+                }
+                return Promise.resolve({ token: res.token });
+            })
+            .catch((e) => {
+                const error = get(e, 'errors.error._schema[0]');
+                const errorMsg = error || 'There was a problem creating your account.';
 
-            return Promise.reject({ errors: { error: errorMsg }});
-        });
+                return Promise.reject({ errors: { error: errorMsg } });
+            });
     };
 
     login = (email, password, communityId) => {
         if (MOCKY) {
-            return Promise.resolve({token: 'FAKETOKEN'});
+            return Promise.resolve({ token: 'FAKETOKEN' });
         }
         return API.login(email, password, communityId).then((res) => {
-            if (res.errors) return Promise.reject({errors: res.errors});
-            return Promise.resolve({token: res.token});
+            if (res.errors) return Promise.reject({ errors: res.errors });
+            return Promise.resolve({ token: res.token });
         });
     };
 
@@ -38,7 +40,7 @@ class Auth {
     };
 
     setSession = (authToken, scope) => {
-        const nowPlus30Days = new Date(Date.now() + (30 * 24 * 60 * 60 * 1000)).getTime();
+        const nowPlus30Days = new Date(Date.now() + 30 * 24 * 60 * 60 * 1000).getTime();
         localStorage.setItem('expires_at', nowPlus30Days);
         localStorage.setItem('access_token', authToken);
         localStorage.setItem('access_scope', scope);
