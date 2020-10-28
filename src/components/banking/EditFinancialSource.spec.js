8de22fc9220aadde2b9b007438b59d40cb4733e2
Chakib <ljazouli.c@gmail.com>
2020-10-28 07:42:22 -0400

    Add tests

--- src/components/banking/EditFinancialSource.spec.js
+++ src/components/banking/EditFinancialSource.spec.js
@@ -4,6 +4,7 @@ import { shallow } from 'enzyme';
 import API from 'app/api';
 import { EditFinancialSource } from './EditFinancialSource';
 import { FINANCIAL_STREAM_STATUS_PENDING } from 'app/constants';
+import GenericFormMessage from 'components/common/GenericFormMessage';
 
 let defaultProps;
 beforeEach(() => {
@@ -103,4 +104,68 @@ it('onSubmit submits correct form data', async () => {
     expect(formData.getAll('1[]')).toEqual(['file']);
     expect(formData.get('adjusted_amount')).toBe('0');
     expect(formData.get('status')).toBe(String(FINANCIAL_STREAM_STATUS_PENDING));
+});
+
+it('sets initial values', async () => {
+    API.getFinancialSource = jest.fn().mockReturnValue({
+        uploaded_documents: [
+            {
+                id: 3,
+                type: {
+                    id: 1,
+                    label: 'W2',
+                },
+                filename: 'w2-1.pdf',
+            },
+            {
+                id: 4,
+                type: {
+                    id: 1,
+                    label: 'W2',
+                },
+                filename: 'w2-2.pdf',
+            },
+            {
+                id: 5,
+                type: {
+                    id: 2,
+                    label: '3 recent paystubs',
+                },
+                filename: 'paystub1.pdf',
+            },
+        ],
+    });
+    const wrapper = await shallow(<EditFinancialSource {...defaultProps} />);
+    expect(wrapper.instance().initialValues['uploadedDocuments']).toEqual({
+        1: {
+            files: [
+                { name: 'w2-1.pdf', id: 3 },
+                { name: 'w2-2.pdf', id: 4 },
+            ],
+            label: 'W2',
+        },
+        2: { files: [{ name: 'paystub1.pdf', id: 5 }], label: '3 recent paystubs' },
+    });
+});
+
+it('Case onError called', async () => {
+    API.getFinancialSource = jest.fn().mockReturnValue({
+        uploaded_documents: [
+            {
+                id: 4,
+                type: {
+                    id: 1,
+                    label: 'W2',
+                },
+                filename: 'w2-2.pdf',
+            },
+        ],
+    });
+    API.updateFinancialSource = jest.fn();
+    const wrapper = await shallow(<EditFinancialSource {...defaultProps} />);
+    wrapper.instance().onError();
+    expect(wrapper.find(GenericFormMessage).length).toBe(1);
+    expect(wrapper.find(GenericFormMessage).prop('messages')).toContain(
+        'Oops! We had some trouble uploading your files. Please try again in a little bit.'
+    );
 });
