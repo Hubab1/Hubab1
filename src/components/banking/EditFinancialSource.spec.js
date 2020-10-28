29fb95bf305475311bdbd8556677380a699da937
Chakib <ljazouli.c@gmail.com>
2020-10-28 07:44:14 -0400

    Remove duplicate test

--- src/components/banking/EditFinancialSource.spec.js
+++ src/components/banking/EditFinancialSource.spec.js
@@ -106,48 +106,6 @@ it('onSubmit submits correct form data', async () => {
     expect(formData.get('status')).toBe(String(FINANCIAL_STREAM_STATUS_PENDING));
 });
 
-it('sets initial values', async () => {
-    API.getFinancialSource = jest.fn().mockReturnValue({
-        uploaded_documents: [
-            {
-                id: 3,
-                type: {
-                    id: 1,
-                    label: 'W2',
-                },
-                filename: 'w2-1.pdf',
-            },
-            {
-                id: 4,
-                type: {
-                    id: 1,
-                    label: 'W2',
-                },
-                filename: 'w2-2.pdf',
-            },
-            {
-                id: 5,
-                type: {
-                    id: 2,
-                    label: '3 recent paystubs',
-                },
-                filename: 'paystub1.pdf',
-            },
-        ],
-    });
-    const wrapper = await shallow(<EditFinancialSource {...defaultProps} />);
-    expect(wrapper.instance().initialValues['uploadedDocuments']).toEqual({
-        1: {
-            files: [
-                { name: 'w2-1.pdf', id: 3 },
-                { name: 'w2-2.pdf', id: 4 },
-            ],
-            label: 'W2',
-        },
-        2: { files: [{ name: 'paystub1.pdf', id: 5 }], label: '3 recent paystubs' },
-    });
-});
-
 it('Case onError called', async () => {
     API.getFinancialSource = jest.fn().mockReturnValue({
         uploaded_documents: [
