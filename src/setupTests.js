6265c593b76cef029c1329df6c04fcd832d866fa
JimVercoelen <jimvercoelen@gmail.com>
2020-11-18 11:35:06 +0100

    rand pretty quick

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,4 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import 'utils/configureYup';
 
-configure({adapter: new Adapter()});
+configure({ adapter: new Adapter() });
