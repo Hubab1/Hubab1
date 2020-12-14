37b2fcacf7b8a4166f64280f281a4780a1a6eac6
Dan Hasday <dhasday@gmail.com>
2020-12-14 11:46:17 -0500

    NESTIO-18118 Update Woodhouse to reference the new constant values
    Updating to use new constants for milestones

--- src/components/AppCancelled.spec.js
+++ src/components/AppCancelled.spec.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { MILESTONE_LEASE_SENT } from 'app/constants';
 import { AppCancelled } from './AppCancelled';
-
 
 const buildProps = (buildingName = 'Fake Building', streetAddress = '123 Fake Street', unitNumber = '2B') => {
     return {
         profile: {
-            events: [{event: '270'}],
+            events: [{ event: MILESTONE_LEASE_SENT.toString() }],
             unit: {
                 unit_number: unitNumber,
             },
@@ -15,13 +15,13 @@ const buildProps = (buildingName = 'Fake Building', streetAddress = '123 Fake St
             community: {
                 building_name: buildingName,
                 normalized_street_address: streetAddress,
-                contact_phone: '1234445510'
-            }
+                contact_phone: '1234445510',
+            },
         },
     };
 };
 
 it('matches snapshot', () => {
-    const wrapper = shallow(<AppCancelled {...buildProps()}/>);
+    const wrapper = shallow(<AppCancelled {...buildProps()} />);
     expect(wrapper.getElement()).toMatchSnapshot();
 });
