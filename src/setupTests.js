27ea629885bbfe839f6c4612732c652191c671c2
JimVercoelen <jimvercoelen@gmail.com>
2020-12-14 13:12:00 +0100

    Updated setup test

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,48 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import enableHooks from 'jest-react-hooks-shallow';
 import 'utils/configureYup';
 
 configure({ adapter: new Adapter() });
+enableHooks(jest, { dontMockByDefault: true });
+
+// Note: to prevent the following warning: "Warning: useLayoutEffect does nothing on the server.."
+jest.mock('react', () => ({
+    ...jest.requireActual('react'),
+    useLayoutEffect: jest.requireActual('react').useEffect,
+}));
+
+const setupGoogleMock = () => {
+    const google = {
+        maps: {
+            places: {
+                AutocompleteService: class {},
+                PlacesServiceStatus: {
+                    INVALID_REQUEST: 'INVALID_REQUEST',
+                    NOT_FOUND: 'NOT_FOUND',
+                    OK: 'OK',
+                    OVER_QUERY_LIMIT: 'OVER_QUERY_LIMIT',
+                    REQUEST_DENIED: 'REQUEST_DENIED',
+                    UNKNOWN_ERROR: 'UNKNOWN_ERROR',
+                    ZERO_RESULTS: 'ZERO_RESULTS',
+                },
+            },
+            Geocoder: class {},
+            GeocoderStatus: {
+                ERROR: 'ERROR',
+                INVALID_REQUEST: 'INVALID_REQUEST',
+                OK: 'OK',
+                OVER_QUERY_LIMIT: 'OVER_QUERY_LIMIT',
+                REQUEST_DENIED: 'REQUEST_DENIED',
+                UNKNOWN_ERROR: 'UNKNOWN_ERROR',
+                ZERO_RESULTS: 'ZERO_RESULTS',
+            },
+        },
+    };
+
+    global.window.google = google;
+};
+
+beforeAll(() => {
+    setupGoogleMock();
+});
