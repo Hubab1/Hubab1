2f7dbc6bd17c16ab6da6fa961e1f46c9765b4852
JimVercoelen <jimvercoelen@gmail.com>
2020-12-15 12:43:02 +0100

    removed support testing hooks - not needed in the end

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,7 +1,5 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import enableHooks from 'jest-react-hooks-shallow';
 import 'utils/configureYup';
 
 configure({ adapter: new Adapter() });
-enableHooks(jest, { dontMockByDefault: true });
