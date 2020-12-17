9292120c8d324c9840714aadeb4c7b9001dd2c21
Dan Hasday <dhasday@gmail.com>
2020-12-17 13:51:08 -0500

    NESTIO-18831 Updating deploy to pull secrets from SSM instead of committing them

--- .buildkite/steps/should-deploy.sh
+++ .buildkite/steps/should-deploy.sh
@@ -10,7 +10,7 @@ APP='woodhouse'
 main() {
     if [[ "${BUILDKITE_BRANCH}" == 'master' ]]; then
         echo "Adding production deploy steps."
-        buildkite-agent pipeline upload .buildkite/deploy.yml
+        buildkite-agent pipeline upload .buildkite/deploy-production.yml
 
         # Alert the user who triggered this build that it's ready to deploy
         # nestctl broadcast-deploy-ready
