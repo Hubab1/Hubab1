8aaf92e755bda69b1ad6755f447ba9cf06160cf1
JimVercoelen <jimvercoelen@gmail.com>
2021-01-04 11:28:27 +0100

    Moved config sentry to sentry util file and added set sentry user and log to sentry utils

--- src/index.js
+++ src/index.js
@@ -5,23 +5,17 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import TagManager from 'react-gtm-module';
 
-// Sentry
-import 'app/configureSentry';
-
-// Yup
+import { setupSentry } from 'utils/sentry';
 import 'utils/configureYup';
-
-// LD
 import 'app/configureLD';
-
 import store from 'reducers/store';
 import App from 'app/App';
 
-const GTM_AUTH = process.env.GTM_AUTH;
+setupSentry();
 
 const tagManagerArgs = {
     gtmId: 'GTM-W8J3S8J',
-    auth: GTM_AUTH,
+    auth: process.env.GTM_AUTH,
 };
 
 TagManager.initialize(tagManagerArgs);
