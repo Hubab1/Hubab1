560067d944a58d1a760d5bb3f2c9fa57a37a9132
Chakib <ljazouli.c@gmail.com>
2021-01-16 22:54:54 -0500

    fix todo for moving HS client id to env file

--- src/utils/hsclient.js
+++ src/utils/hsclient.js
@@ -1,6 +1,5 @@
 import HelloSign from 'hellosign-embedded';
-import { HELLOSIGN_CLIENT_ID } from 'app/constants';
 
 export default new HelloSign({
-    clientId: HELLOSIGN_CLIENT_ID,
+    clientId: process.env.HELLOSIGN_CLIENT_ID,
 });
