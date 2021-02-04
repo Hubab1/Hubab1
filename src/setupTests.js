7167bddeb5230eb93971639aeca838242abfae4d
JimVercoelen <jimvercoelen@gmail.com>
2021-02-04 13:14:54 +0100

    resolved useLayoutEffect warning

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,7 +1,11 @@
+import React from 'react';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import enableHooks from 'jest-react-hooks-shallow';
 import 'utils/configureYup';
+
+// https://stackoverflow.com/questions/58070996/how-to-fix-the-warning-uselayouteffect-does-nothing-on-the-server
+React.useLayoutEffect = React.useEffect;
 
 configure({ adapter: new Adapter() });
 enableHooks(jest, { dontMockByDefault: true });
