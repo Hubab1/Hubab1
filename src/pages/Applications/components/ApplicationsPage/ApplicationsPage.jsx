fe5bbe98311da08193eb3151b20f3b66f8dde189
JimVercoelen <jimvercoelen@gmail.com>
2021-02-20 16:48:25 +0100

    NESTIO-19345 use page components on applications page with for now no content

--- src/pages/Applications/components/ApplicationsPage/ApplicationsPage.jsx
+++ src/pages/Applications/components/ApplicationsPage/ApplicationsPage.jsx
@@ -1,13 +1,42 @@
 import React from 'react';
+// TODO: will be used in follow up PR
 // eslint-disable-next-line no-unused-vars
 import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core';
+
+import Page from 'components/common/Page/Page';
+import { H3 } from 'assets/styles';
+
+const useStyles = makeStyles(() => ({
+    root: {
+        textAlign: 'left',
+    },
+    section: {
+        marginBottom: 32,
+
+        '& h3': {
+            marginBottom: 16,
+        },
+    },
+}));
 
 export function ApplicationsPage() {
-    return <div>Applications Page</div>;
+    const classes = useStyles();
+
+    return (
+        <Page className={classes.root} header="My Applications">
+            <div className={classes.section}>
+                <H3>Active Applications</H3>
+            </div>
+            <div className={classes.section}>
+                <H3>Past Applications</H3>
+            </div>
+        </Page>
+    );
 }
 
 ApplicationsPage.propTypes = {
-    // TODO
+    // TODO: will be used in follow up PR
 };
 
 export default ApplicationsPage;
