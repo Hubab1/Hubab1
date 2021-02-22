8a51d7f07544201489c7863d937e3d8863ad42f1
JimVercoelen <jimvercoelen@gmail.com>
2021-02-22 08:29:13 +0100

    NESTIO-19345 added application component

--- src/assets/theme.js
+++ src/assets/theme.js
@@ -9,7 +9,7 @@ export const createTheme = (config) => {
     const primaryColor = primary_color ? `#${primary_color}` : '#2B44FF';
     const secondaryColor = secondary_color ? `#${secondary_color}` : '#2B44FF';
 
-    return createMuiTheme({
+    const theme = createMuiTheme({
         // Important note: don't set palette.type to 'dark' based on dark mode
         // This will put all material ui's components in a dark mode state - which will cause a lot of styling issues
         // Instead, we use our this darkMode variable
@@ -30,4 +30,29 @@ export const createTheme = (config) => {
             },
         },
     });
+
+    // Overwrite component styling globally so when using the styling will be consistent
+    theme.overrides = {
+        MuiCardHeader: {
+            title: {
+                fontSize: 16,
+                fontWeight: 600,
+            },
+            subheader: {
+                fontSize: 16,
+                fontWeight: 400,
+            }
+        },
+        // Note: the default padding of the card content is 16px, 16px, 24px, 16px
+        // This override addresses that by forcing it all to 16px
+        MuiCardContent: {
+            root: {
+                '&:last-child': {
+                    padding: `${theme.spacing(2)}px !important`
+                }
+            }
+        },
+    };
+
+    return theme;
 };
