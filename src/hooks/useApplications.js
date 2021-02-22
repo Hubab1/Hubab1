c11800b5bda9dbf7ce9f970c1bc15150c6da19ea
JimVercoelen <jimvercoelen@gmail.com>
2021-02-22 15:04:26 +0100

    NESTIO-19345 code cleanup

--- src/hooks/useApplications.js
+++ src/hooks/useApplications.js
@@ -1,10 +1,10 @@
 import useEnhancedFetch from 'hooks/useEnhancedFetch';
 import { API_URL } from 'config';
 
-export const GET_APPLICATIONs = `${API_URL}/applications`;
+export const GET_APPLICATIONS = `${API_URL}/applications`;
 
 const useApplications = () => {
-    const { loading, error, data, ...rest } = useEnhancedFetch(GET_APPLICATIONs, {
+    const { loading, error, data, ...rest } = useEnhancedFetch(GET_APPLICATIONS, {
         method: 'GET',
         fetchOnDemand: false,
         cachePolicy: 'network-only',
