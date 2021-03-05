e16112690821295601c99c7a9063391cd0625c0a
JimVercoelen <jimvercoelen@gmail.com>
2021-03-05 16:52:36 +0100

    NESTIO-20246 code improvement: reuse same util

--- src/pages/Banking/pages/IncomeVerificationSummary/components/IncomeVerificationSummaryPage/IncomeVerificationSummaryPage.jsx
+++ src/pages/Banking/pages/IncomeVerificationSummary/components/IncomeVerificationSummaryPage/IncomeVerificationSummaryPage.jsx
@@ -31,6 +31,7 @@ import BackLink from 'components//BackLink/BackLink';
 import ExistingItemsExpansionPanel from 'components//ExistingItemsExpansionPanel/ExistingItemsExpansionPanel';
 import ResetApplicantFinancials from 'pages/Banking/pages/IncomeVerificationSummary/components/ResetApplicantFinancials/ResetApplicantFinancials';
 import BankingContext from 'pages/Banking/BankingContext';
+import { getIncompleteFinancialSourceWarning } from 'pages/Banking/bankinUtils';
 
 import { H1, H3 } from 'assets/styles';
 import { styles, Spacer, infoIconRoot } from 'assets/styles';
@@ -81,28 +82,6 @@ const incomeOrAssetItemWarning = css`
         color: #fb6d68;
     }
 `;
-
-export const getIncompleteFinancialSourceWarning = (source, isAsset) => {
-    const { adjusted_amount, status } = source;
-    const isIncomplete = status === FINANCIAL_STREAM_STATUS_INCOMPLETE;
-
-    if (!isIncomplete) return null;
-
-    let warning;
-    if (adjusted_amount) {
-        if (isAsset) {
-            warning = `The documents for this asset source show a value of ${prettyCurrency(adjusted_amount)}.`;
-        } else {
-            warning = `The documents for this income source show earnings of ${prettyCurrency(adjusted_amount)}/year.`;
-        }
-    } else {
-        warning = `This ${
-            isAsset ? 'asset' : 'income'
-        } source has been marked as having incorrect or insufficient documents.`;
-    }
-
-    return warning;
-};
 
 export function IncomeOrAssetItemWarning({ source, isAsset }) {
     const warning = getIncompleteFinancialSourceWarning(source, isAsset);
