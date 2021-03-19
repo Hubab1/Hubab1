418722d4512fab764798baacb0a6f9ddcbd57a5f
bynarlogic <joshua.jarvis@funnelleasing.com>
2021-03-19 16:18:51 -0400

    Updated ConnectBankPage.componentWillUnmount() to call window.finicityConnect rather than Window.finicityConnect. Also, we should be calling destroyIFrame() rather than destroy().

--- src/pages/Banking/pages/ConnectBankPage.jsx
+++ src/pages/Banking/pages/ConnectBankPage.jsx
@@ -32,8 +32,8 @@ export class ConnectBankPage extends React.Component {
     componentWillUnmount() {
         clearInterval(window.fetchReportsInterval);
         try {
-            if (Window.finicityConnect) {
-                Window.finicityConnect.destroy();
+            if (window.finicityConnect) {
+                window.finicityConnect.destroyIFrame();
             }
         } catch (e) {
             console.error(e);
@@ -171,6 +171,9 @@ export class ConnectBankPage extends React.Component {
     };
 
     render() {
+        const finicityConnect = window.finicityConnect;
+        console.log({ finicityConnect });
+
         if (!this.props.applicant) {
             return <div />;
         }
