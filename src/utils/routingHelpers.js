1bf1a10e40569adf569e6b5e68bd2d664b6a0bc4
JimVercoelen <jimvercoelen@gmail.com>
2021-04-15 14:13:45 +0200

    NESTIO-21191 added description

--- src/utils/routingHelpers.js
+++ src/utils/routingHelpers.js
@@ -10,6 +10,24 @@ export const getApplicationIdFromUrl = () => {
     return applicationIdFromUrl;
 };
 
+/**
+ * Get Initial route
+ *  - When we do know what application the applicant is trying to access
+ *    (the application id is part of the url or is present in the configuration).
+ *    Then we return to the initial page of that application.
+ *
+ *  - When the applicant has multiple active applications, and either applied for another one, or got invited to another one.
+ *    Then we return the applications page, where the new application can be started.
+ *
+ *  - When we don't know what application the applicant is trying to access but does have multiple active applications.
+ *    Then we return the applications page so that the applicant can choose the application.
+ *
+ *  - When the applicant is accessing the app with an application of another community.
+ *    Then we return to the applications page, where applicant can switch between community environments.
+ *
+ *  - When none of the above.
+ *    Then we 'fallback' by returning the initial page of the application.
+ */
 export const getInitialRoute = (applicant, configuration, accessedAppByInvitationOrWebsite, initialApplicationPage) => {
     const applicationIdFromUrl = getApplicationIdFromUrl();
     const applicationIdFromConfiguration = configuration?.application_id;
@@ -22,7 +40,7 @@ export const getInitialRoute = (applicant, configuration, accessedAppByInvitatio
         return ROUTES.APPLICATIONS;
     }
 
-    if (!applicationIdFromUrl && applicant?.num_active_applications > 1) {
+    if (!applicationIdFromUrl && !applicationIdFromConfiguration && applicant?.num_active_applications > 1) {
         return ROUTES.APPLICATIONS;
     }
 
